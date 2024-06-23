d159c84a5be3b3aed0ee54ad6f43b76fd5d9594f
Liuba Kuibida <k2.liuba@gmail.com>
2024-06-23 14:27:44 +0200

    add minor style fixes

--- src/App.vue
+++ src/App.vue
@@ -60,84 +60,87 @@ function resetTodo() {
 </script>
 
 <template>
-  <section class="add-container">
-    <h2>New Task</h2>
-    <form @submit.prevent="addTodo()">
-      <label><input
-        v-model="newTodo"
-        name="newTodo"
-        class="new-todo"
-        autocomplete="off"
-      ></label>
-      <fieldset>
-        <legend>Please select your preferred contact method:</legend>
-        <div class="labels">
-          <input
-            id="typeChoice1"
-            v-model="newTodoType"
-            type="radio"
-            name="type"
-            value="Personal"
-            checked
-          >
-          <label for="typeChoice1">Personal</label>
-          <input
-            id="typeChoice2"
-            v-model="newTodoType"
-            type="radio"
-            name="type"
-            value="Work"
-          >
-          <label for="typeChoice2">Work</label>
-          <input
-            id="typeChoice3"
-            v-model="newTodoType"
-            type="radio"
-            name="type"
-            value="Other"
-          >
-          <label for="typeChoice3">Other</label>
+  <div class="app-wrapper">
+    <section class="add-container">
+      <h2>New Task</h2>
+      <form @submit.prevent="addTodo()">
+        <label><input
+          v-model="newTodo"
+          name="newTodo"
+          class="new-todo"
+          autocomplete="off"
+        ></label>
+        <fieldset>
+          <legend>Please select your preferred contact method:</legend>
+          <div class="labels">
+            <input
+              id="typeChoice1"
+              v-model="newTodoType"
+              type="radio"
+              name="type"
+              value="Personal"
+              checked
+            >
+            <label for="typeChoice1">Personal</label>
+            <input
+              id="typeChoice2"
+              v-model="newTodoType"
+              type="radio"
+              name="type"
+              value="Work"
+            >
+            <label for="typeChoice2">Work</label>
+            <input
+              id="typeChoice3"
+              v-model="newTodoType"
+              type="radio"
+              name="type"
+              value="Other"
+            >
+            <label for="typeChoice3">Other</label>
+          </div>
+        </fieldset>
+        <button class="add-btn">
+          Add task
+        </button>
+      </form>
+    </section>
+    <template v-if="todos.length">
+      <section class="review-container">
+        <h2>ToDo List</h2>
+        <div class="tags-wrapper">
+          <p v-for="(filter, index) in filters" :key="index">
+            <span
+              :class="{ active: filter === activeFilter }"
+              @click="filterTodo(filter)"
+            >{{ filter }}</span>
+          </p>
         </div>
-      </fieldset>
-      <button class="add-btn">
-        Add task
-      </button>
-    </form>
-  </section>
-  <template v-if="todos.length">
-    <section class="review-container">
-      <h2>ToDo List</h2>
-      <div class="tags-wrapper">
-        <p v-for="(filter, index) in filters" :key="index">
-          <span
-            :class="{ active: filter === activeFilter }"
-            @click="filterTodo(filter)"
-          >{{ filter }}</span>
+        <p v-if="!getTodos.length" class="empty-message">
+          {{ emptyNote }}
         </p>
-      </div>
-      <p v-if="!getTodos.length" class="empty-message">
-        {{ emptyNote }}
-      </p>
-      <ul v-else>
-        <li
-          v-for="(todo, index) in getTodos"
-          :key="index"
-          :class="{ done: todo.done }"
+        <ul v-if="getTodos.length" class="todo-list">
+          <li
+            v-for="(todo, index) in getTodos"
+            :key="index"
+            class="todo-item"
+            :class="{ done: todo.done }"
+          >
+            <span class="task" @click="doneTodo(todo)">{{ todo.content }}</span>
+            <span class="label">{{ todo.type }}</span>
+            <button class="remove-btn" @click="removeTodo(todo.id)" />
+          </li>
+        </ul>
+      </section>
+    </template>
+    <template v-else>
+      <section class="empty-container">
+        <h2>Your tasks will appear here</h2>
+        <img
+          src="https://doodleipsum.com/700?i=b2a7f828acad27e884b0aabcf6f10ab6"
+          alt="a picture of man relaxing and drinking tea"
         >
-          <span class="task" @click="doneTodo(todo)">{{ todo.content }}</span>
-          <span class="label">{{ todo.type }}</span>
-          <button class="remove-btn" @click="removeTodo(todo.id)" />
-        </li>
-      </ul>
-    </section>
-  </template>
-  <template v-else>
-    <section class="empty-container">
-      <h2>Your tasks will appear here</h2>
-      <img
-        src="https://doodleipsum.com/700?i=b2a7f828acad27e884b0aabcf6f10ab6"
-        alt="a picture of man relaxing and drinking tea"
-      >
-    </section>
-  </template>
+      </section>
+    </template>
+  </div>
 </template>
